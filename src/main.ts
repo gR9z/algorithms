1c81a1df0e3b2b705d3a931960f4155983405b1d
Royer Gaétan <royergaetan90@gmail.com>
2024-01-16 18:01:31 +0100

    Change static array to dynamically generated array

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import bubbleSort from './algorithms/bubbleSort';
 import quickSortAlgorithm from './algorithms/quickSort';
 import recursiveSum from './algorithms/recursivity';
 
-const arr = [128, 15, 2, 44, 25, 19, 5, 18, 20, 99];
+const arr = Array.from({ length: 100 }, () => Math.floor(Math.random() * 101));
 
 display('bubbleSort', bubbleSort, arr);
 display('quickSort', quickSortAlgorithm, arr);
